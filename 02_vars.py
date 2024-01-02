65c6675115f5f132a33017bb6db1912332e1e5fe
tonny187 <tonyoanthony@gmail.com>
2024-01-02 18:26:30 -0500

    second commit

---
+++ 02_vars.py
@@ -0,0 +1,7 @@
+my_name = "Anthony"
+
+print("Hola, yo soy ", my_name)
+
+your_name = input("¿Cual es tu nombre? ")
+print("Hola" , your_name , "encantado de conocerte")
+
---
+++ 03_types.py
@@ -0,0 +1,22 @@
+#string
+my_name = "Anthony"
+print("my name ==>", my_name)
+
+print(type(my_name))
+
+#int
+my_age = 32
+print("my age ==>", my_age)
+
+print(type(my_age))
+
+#booleans
+is_single = False
+print("is single ==>", is_single)
+
+print(type(is_single))
+
+#input
+your_age = input("¿Cual es tu edad?")
+print("Tu edad es", your_age)
+print(type(your_age))
