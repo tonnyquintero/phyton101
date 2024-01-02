15265487d070e6ca27b757208a709716d191ed31
tonny187 <tonyoanthony@gmail.com>
2024-01-02 17:40:55 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+print("hello world")
+print("im coding with python")
+print(12 + 5)
+print(12 - 9)
+# Esto es un comentario..
+print(2 * 5)
+print(12 / 5)
+
+'''
+Esto es un comentario
+de varias
+lineas
+'''
